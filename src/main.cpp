d6ef40c054bab4bde1fef0573302bcb1b9e86841
Дмитрий <53074863+1Kuso4ek1@users.noreply.github.com>
2021-03-08 14:18:10 +0300

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -76,13 +76,8 @@ void build() {
 	for(int i = 0; i < flags.size(); i++) command += "-" + flags[i] + " ";
 	for(int i = 0; i < files.size(); i++) {
 		if(files[i].find(".h") != std::string::npos)
-			if(files[i].find("/") != std::string::npos)
-				command += "\"" + files[i].substr(files[i].find("/") + 1, files[i].size()) + ".gch" + "\" ";
-			else
-				command += "\"" + files[i] + ".gch" + "\" ";
-		else if(files[i].find("/") != std::string::npos)
-			command += "\"" + (files[i].substr(files[i].find("/") + 1, files[i].find(".") - 3) + "o") + "\" ";
-		else 
+			command += "\"" + files[i] + ".gch" + "\" ";
+		else
 			command += "\"" + (files[i].substr(0, files[i].find(".") + 1) + "o") + "\" ";
 	}
 	for(int i = 0; i < libs.size(); i++) command += " -l" + libs[i];
@@ -112,7 +107,7 @@ int main(int argc, char* argv[]) {
 		std::cout << "\033[31mCan't detect GCC compiler!\033[0m \033[32mInstalling...\033[0m" << std::endl;
 #ifdef _WIN64
 		std::cout << "To install gcc download this file, follow the instructions, then reboot. Link - ftp://ftp.equation.com/gcc/gcc-10.2.0-64.exe" << std::endl;
-#elifdef _WIN32 
+#elif _WIN32 
 		std::cout << "To install gcc download this file, follow the instructions, then reboot. Link - ftp://ftp.equation.com/gcc/gcc-10.2.0-32.exe" << std::endl;
 #else
 		system("sudo apt install g++");

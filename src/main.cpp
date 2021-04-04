d33cdcabe9e6dde3c529233d4b87a80b1074f708
Дмитрий <53074863+1Kuso4ek1@users.noreply.github.com>
2021-04-04 21:10:28 +0300

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -75,7 +75,7 @@ void build() {
 	{
 		if(files[i].find("/") != std::string::npos)
 		{
-			files[i] = files[i].substr(files[i].find("/") + 1, files[i].size());
+			files[i] = files[i].substr(files[i].find_last_of("/") + 1, files[i].size());
 		}
 	}
 	std::string command = "g++ ";
@@ -114,7 +114,7 @@ int main(int argc, char* argv[]) {
 		std::cout << "\033[31mCan't detect GCC compiler!\033[0m \033[32mInstalling...\033[0m" << std::endl;
 #ifdef _WIN64
 		std::cout << "To install gcc download this file, follow the instructions, then reboot. Link - ftp://ftp.equation.com/gcc/gcc-10.2.0-64.exe" << std::endl;
-#elif _WIN32 
+#elifdef _WIN32 
 		std::cout << "To install gcc download this file, follow the instructions, then reboot. Link - ftp://ftp.equation.com/gcc/gcc-10.2.0-32.exe" << std::endl;
 #else
 		system("sudo apt install g++");
